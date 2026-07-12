b45d9abb52dc1a660ae802f5979445b6a0e2a887
Netsai <hellonetsai@gmail.com>
2026-07-12 18:33:22 +0200

    Connect research database to website

--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Research(models.Model):
+
+    title = models.CharField(max_length=200)
+
+    description = models.TextField()
+
+    publication_date = models.DateField()
+
+    report = models.FileField(
+        upload_to="research_reports/",
+        blank=True,
+        null=True
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+
+    def __str__(self):
+        return self.title
