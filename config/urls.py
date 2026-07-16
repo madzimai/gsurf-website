67c1f8410d7f525f3a041eaaab8e65b873ffed04
Netsai <hellonetsai@gmail.com>
2026-07-16 10:33:38 +0200

    Robbot chat -XML Sitemap

--- config/urls.py
+++ config/urls.py
@@ -3,11 +3,32 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
 
+from core.sitemaps import StaticViewSitemap
+
+itemaps = {
+
+    'static': StaticViewSitemap(),
+
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+  
+
+path(
+
+'sitemap.xml',
+
+sitemap,
+
+{'sitemaps': sitemaps},
+
+name='sitemap'
+
+),
 ]
 
 
